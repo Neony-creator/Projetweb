1330e63cdd5b4c2898bc219e6a66ee2a4d1b3257
Neony-creator <93582132+Neony-creator@users.noreply.github.com>
2022-03-22 11:25:44 +0100

    chaine connexion

---
+++ projet/site/chaineconnexion.php
@@ -0,0 +1,8 @@
+
+
+<?php
+$dsn = 'mysql:dbname=projetweb;host=127.0.0.1:3306';                /*Chaine de connexion avec IP et BDD */
+$username_bdd = "root";                                                 /*Nom d'utilisateur pour MySQL */
+$password_bdd = "CESI123";                                               /*Mot de passe pour MySQL*/
+$error = false;                                                         /*Erreur de connexion à false avant connexion*/
+
